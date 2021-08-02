5dba99c6d5fbe60793d4e34ee14aa5fbf9ea6242
Farhad <snaptrap.org@gmail.com>
2021-08-03 07:15:19 +0800

    Changed to request module

--- request.py
+++ request.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-def fetch_data(url):
+def fetch_data(url: str):
     req = requests.get(url)
     soup = BeautifulSoup(req.text, 'xml')
     items = soup.find_all('item')
-
     keys = []
     values = []
 
-    # Lopp all subelement to get title(prayer name) and description(prayer time)
+    # Loop all subelement to get title(prayer name) and description(prayer time)
     for item in items:
         title = item.find_all('title')
         desc = item.find_all('description')
@@ -18,7 +17,8 @@ def fetch_data(url):
             keys.append(i.text) # Append to keys list
 
         for i in desc:
-            values.append(i.text)   # Append to values list
+            time = i.text.split(":", 2)
+            values.append("{}:{}".format(time[0], time[1]))   # Append to values list
 
     return dict(zip(keys, values)) 
 
